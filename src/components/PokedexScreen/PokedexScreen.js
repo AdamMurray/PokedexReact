a6040b2a2266dfd750b1232cfb78ea3ce088c9fc
Adam <adam.john.campbell.murray@gmail.com>
2016-09-26 23:37:03 +0100

    Update component styles

--- src/components/PokedexScreen/PokedexScreen.js
+++ src/components/PokedexScreen/PokedexScreen.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import './PokedexScreen.css';
 
+// The screen height is the height of the window minus the TopBar height
+const screenHeight = window.innerHeight - 80;
+const screenStyle = {
+  height: screenHeight
+};
+
 class PokedexScreen extends Component {
   render() {
     return (
-      <Row className="PokedexScreen">
+      <Row className="PokedexScreen" style={screenStyle}>
         <Col xs={12} className="PokedexScreen-inner">
           <Row className="PokedexScreen-screen">
             <Col xs={12}>
