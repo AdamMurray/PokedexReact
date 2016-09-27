7462c4f9183ef2cb36fc534f6e78f72a3a1f58f2
Adam <adam.john.campbell.murray@gmail.com>
2016-09-27 20:46:25 +0100

    Fix build again

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,19 @@ import './App.css';
 import Pokedex from './Pokedex/Pokedex';
 
 class App extends Component {
+
   render() {
+
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('sw.js').then(function (registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }).catch(function (err) {
+        // registration failed
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    }
+
     return (
       <div>
         <Pokedex />
---
+++ sw.js
@@ -0,0 +1,3 @@
+(function () {
+  console.log('Service worker starting...');
+})();
